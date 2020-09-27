8b8a632c5a792c16f82b4a777b21d4dad6bfc04d
lewis <lingyidx@gmail.com>
2020-09-27 23:25:25 +0800

    fix: add try-lock\unlock

--- src/test/java/com/github/hcsp/redis/TestMain.java
+++ src/test/java/com/github/hcsp/redis/TestMain.java
@@ -3,6 +3,9 @@ package com.github.hcsp.redis;
 import redis.clients.jedis.Jedis;
 import redis.clients.jedis.JedisPool;
 
+import java.util.concurrent.TimeUnit;
+import java.util.concurrent.locks.LockSupport;
+
 public class TestMain {
     public static void main(String[] args) throws Exception {
         JedisPool pool = new JedisPool();
@@ -11,16 +14,31 @@ public class TestMain {
                 .runUnderLock(
                         () -> {
                             try (Jedis jedis = pool.getResource()) {
+                                // try lock
+                                tryLock(jedis);
                                 String value = jedis.get("KeyUnderTest");
                                 if (value == null) {
                                     value = "1";
                                 } else {
                                     value = "" + (Integer.parseInt(value) + 1);
                                 }
-                                Thread.sleep(100);
                                 jedis.set("KeyUnderTest", value);
+
+                                // unlock
+                                unlock(jedis);
                                 return null;
                             }
                         });
+    }
+
+    private static void unlock(Jedis jedis) {
+        jedis.del("distribute_jvm_share_key");
+    }
+
+    private static void tryLock(Jedis jedis) {
+        while (jedis.setnx("distribute_jvm_share_key", "0") == 0L) {
+            LockSupport.parkNanos(TimeUnit.MILLISECONDS.toNanos(100L));
+            System.out.println(Thread.currentThread().getName() + "try to get lock");
+        }
     }
 }
