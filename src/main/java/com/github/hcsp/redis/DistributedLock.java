1604514bed3a35079da805f21febc1835db7821c
SelfRidicule <selfridicule@gmail.com>
2020-11-03 19:22:31 +0800

    redis 分布式锁

--- src/main/java/com/github/hcsp/redis/DistributedLock.java
+++ src/main/java/com/github/hcsp/redis/DistributedLock.java
@@ -1,10 +1,17 @@
 package com.github.hcsp.redis;
 
+import redis.clients.jedis.Jedis;
+import redis.clients.jedis.JedisPool;
+
+import java.lang.management.ManagementFactory;
 import java.util.concurrent.Callable;
 
 public class DistributedLock {
-    /** The lock name. A lock with same name might be shared in multiple JVMs. */
+    /**
+     * The lock name. A lock with same name might be shared in multiple JVMs.
+     */
     private String name;
+    private static final long TIME_OUT = 1000;
 
     public DistributedLock(String name) {
         this.name = name;
@@ -14,10 +21,34 @@ public class DistributedLock {
      * Run a given action under lock.
      *
      * @param callable the action to be executed
-     * @param <T> return type
+     * @param <T>      return type
      * @return the result
      */
     public <T> T runUnderLock(Callable<T> callable) throws Exception {
-        return callable.call();
+        JedisPool pool = new JedisPool();
+        try (Jedis jedis = pool.getResource()) {
+            String timestamp = Long.toString(System.currentTimeMillis());
+            long time = 0;
+            // 获取锁
+            while (time < TIME_OUT) {
+                long setResult = jedis.setnx(name, timestamp);
+                if (setResult == 1) {
+                    // 执行
+                    T result = callable.call();
+                    // 释放锁
+                    jedis.del(name);
+                    System.out.println("执行完毕...");
+                    return result;
+                } else {
+                    time += 10;
+                    Thread.sleep(10);
+                }
+            }
+        }
+        System.out.println("获取锁失败超时...");
+        return null;
     }
+
+
+
 }
